ab41b0100c8e4b5fe20232b0097ddeb319ba1c28
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-09-25 06:41:30 -0400

    created utils folder and custom css file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
+import React from 'react';
 import './App.css';
+
+import About from './components/About'
+import Banner from './components/Banner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <About></About>
+        <Banner></Banner>
+      </main>
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section>
+      <p>
+      I am currently a Police Sergeant looking to switch into a technology career. I always had the love for coding ever since the MySpace days. I enjoyed creating web-pages that were appealing to the user eyes. Web Development allows me to be creative and challenges my knowledge while still having fun. In addition, I'm still able to solve problems but in the technical world!
+      </p>
+      <br />
+      <p>
+        With that being said, I am a full-stack web developer with a business administration background looking to contribute my newly developed technical skills acquired from the Rutgers University Coding Boot Camp. I have strong skills in creativity, teamwork, and meeting deadlines. My ability to quickly learn cutting-edge web technologies, combined with my adept problem-solving abilities and determination, make me a strong addition to any software engineering team.
+     </p>
+    </section>
+  )
+}
+
+export default About
